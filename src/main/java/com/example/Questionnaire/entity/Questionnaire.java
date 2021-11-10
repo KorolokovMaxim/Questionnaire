f5f91a19bfffff9ec2f0543c41497761991dfc26
Maxim Korolkov <ratdeputat@gmail.com>
2021-11-10 23:09:15 +0400

    create entity Questionnaire

---
+++ src/main/java/com/example/Questionnaire/entity/Questionnaire.java
@@ -0,0 +1,62 @@
+package com.example.Questionnaire.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "questionnaire" )
+public class Questionnaire {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "questionnaire", orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
+    public Questionnaire() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Questionnaire that = (Questionnaire) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(questions, that.questions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, questions);
+    }
+}
