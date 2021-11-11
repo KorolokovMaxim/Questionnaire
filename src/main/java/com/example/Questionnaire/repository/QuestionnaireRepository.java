813e678f853403c2407cc50cb4be6b594e60e244
Maxim Korolkov <ratdeputat@gmail.com>
2021-11-11 23:55:13 +0400

    create Questionnaire repo

---
+++ src/main/java/com/example/Questionnaire/repository/QuestionnaireRepository.java
@@ -0,0 +1,11 @@
+package com.example.Questionnaire.repository;
+
+import com.example.Questionnaire.entity.Questionnaire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionnaireRepository  extends JpaRepository<Questionnaire, Long> {
+
+
+    Questionnaire findByName(String name);
+
+}
