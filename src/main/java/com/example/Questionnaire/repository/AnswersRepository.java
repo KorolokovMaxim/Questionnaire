7c0ebdfac3d7db3cb4b3d735e0314fc408aaf49c
Maxim Korolkov <ratdeputat@gmail.com>
2021-11-14 23:39:30 +0400

    add method for search answers by question

--- src/main/java/com/example/Questionnaire/repository/AnswersRepository.java
+++ src/main/java/com/example/Questionnaire/repository/AnswersRepository.java
@@ -1,10 +1,15 @@
 package com.example.Questionnaire.repository;
 
 import com.example.Questionnaire.entity.Answers;
+import com.example.Questionnaire.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AnswersRepository extends JpaRepository<Answers,Long> {
 
     Answers findByName(String name);
+
+    List<Answers> findAnswersByQuestion(Question question);
 
 }
