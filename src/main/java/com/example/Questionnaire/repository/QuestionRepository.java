849c3c7c5af38c3c709a34e50ddb6c5217366c46
Maxim Korolkov <ratdeputat@gmail.com>
2021-11-14 23:40:44 +0400

    add method find quest in questionnaire

--- src/main/java/com/example/Questionnaire/repository/QuestionRepository.java
+++ src/main/java/com/example/Questionnaire/repository/QuestionRepository.java
@@ -1,10 +1,15 @@
 package com.example.Questionnaire.repository;
 
 import com.example.Questionnaire.entity.Question;
+import com.example.Questionnaire.entity.Questionnaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
 
     Question findByName(String name);
+
+    List<Question> findByQuestionnaire(Questionnaire q);
 
 }
