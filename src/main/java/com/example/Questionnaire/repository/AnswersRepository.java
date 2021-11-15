e59302770ed390e0f6f83bd46413a874a9f4f42f
Maxim Korolkov <ratdeputat@gmail.com>
2021-11-15 23:54:04 +0400

    add method find all answers for question

--- src/main/java/com/example/Questionnaire/repository/AnswersRepository.java
+++ src/main/java/com/example/Questionnaire/repository/AnswersRepository.java
@@ -5,11 +5,15 @@ import com.example.Questionnaire.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Map;
 
 public interface AnswersRepository extends JpaRepository<Answers,Long> {
 
     Answers findByName(String name);
 
     List<Answers> findAnswersByQuestion(Question question);
+
+    List<Answers> findAllByQuestionId(Long id);
+
 
 }
